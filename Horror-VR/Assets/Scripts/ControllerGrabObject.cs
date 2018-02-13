5dd52a9ff5754eaeb8df103bc01b19368bae440b
David Fall <David.fall@students.plymouth.ac.uk>
2018-02-13 23:50:59 +0000

    Hand animation
    rigged and connected a hand animation to the controller input of the vive , more difficult than it sounds

--- Horror-VR/Assets/Scripts/ControllerGrabObject.cs
+++ Horror-VR/Assets/Scripts/ControllerGrabObject.cs
@@ -1,100 +1,106 @@
 ﻿
 
 using UnityEngine;
-
-public class ControllerGrabObject : MonoBehaviour
+namespace control
 {
-    private SteamVR_TrackedObject trackedObj;
-
-    private GameObject collidingObject;
-    private GameObject objectInHand;
-
-    private SteamVR_Controller.Device Controller
+    public class ControllerGrabObject : MonoBehaviour
     {
-        get { return SteamVR_Controller.Input((int)trackedObj.index); }
-    }
+        private SteamVR_TrackedObject trackedObj;
 
-    void Awake()
-    {
-        trackedObj = GetComponent<SteamVR_TrackedObject>();
-    }
-
-
-    public void OnTriggerEnter(Collider other)
-    {
-        SetCollidingObject(other);
-    }
-
-    public void OnTriggerStay(Collider other)
-    {
-        SetCollidingObject(other);
-    }
-
-    public void OnTriggerExit(Collider other)
-    {
-        if (!collidingObject)
+        private GameObject collidingObject;
+        private GameObject objectInHand;
+        private SteamVR_Controller.Device Controller
         {
-            return;
+            get { return SteamVR_Controller.Input((int)trackedObj.index); }
         }
 
-        collidingObject = null;
-    }
-
-    private void SetCollidingObject(Collider col)
-    {
-        if (collidingObject || !col.GetComponent<Rigidbody>())
+        void Awake()
         {
-            return;
+            trackedObj = GetComponent<SteamVR_TrackedObject>();
         }
 
-        collidingObject = col.gameObject;
-    }
 
-    void Update()
-    {
-        if (Controller.GetHairTriggerDown())
+        public void OnTriggerEnter(Collider other)
         {
-            if (collidingObject)
+            SetCollidingObject(other);
+        }
+
+        public void OnTriggerStay(Collider other)
+        {
+            SetCollidingObject(other);
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            if (!collidingObject)
             {
-                GrabObject();
+                return;
+            }
+
+            collidingObject = null;
+        }
+
+        private void SetCollidingObject(Collider col)
+        {
+            if (collidingObject || !col.GetComponent<Rigidbody>())
+            {
+                return;
+            }
+
+            collidingObject = col.gameObject;
+        }
+
+        void Update()
+        {
+            if (Controller.GetHairTriggerDown())
+            {
+
+                Keypress.keypress = true;
+                 Debug.Log("pressed");
+                if (collidingObject)
+                {
+                    GrabObject();
+                }
+            }
+
+            if (Controller.GetHairTriggerUp())
+            {
+                Keypress.keypress = false;
+
+                if (objectInHand)
+                {
+                    ReleaseObject();
+                }
             }
         }
 
-        if (Controller.GetHairTriggerUp())
+        private void GrabObject()
         {
-            if (objectInHand)
+            objectInHand = collidingObject;
+            collidingObject = null;
+            var joint = AddFixedJoint();
+            joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+        }
+
+        private FixedJoint AddFixedJoint()
+        {
+            FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+            fx.breakForce = 20000;
+            fx.breakTorque = 20000;
+            return fx;
+        }
+
+        private void ReleaseObject()
+        {
+            if (GetComponent<FixedJoint>())
             {
-                ReleaseObject();
+                GetComponent<FixedJoint>().connectedBody = null;
+                Destroy(GetComponent<FixedJoint>());
+                objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+                objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
             }
+
+            objectInHand = null;
         }
-    }
-
-    private void GrabObject()
-    {
-        objectInHand = collidingObject;
-        collidingObject = null;
-        var joint = AddFixedJoint();
-        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
-    }
-
-    private FixedJoint AddFixedJoint()
-    {
-        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
-        fx.breakForce = 20000;
-        fx.breakTorque = 20000;
-        return fx;
-    }
-
-    private void ReleaseObject()
-    {
-        if (GetComponent<FixedJoint>())
-        {
-            GetComponent<FixedJoint>().connectedBody = null;
-            Destroy(GetComponent<FixedJoint>());
-            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
-            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
-        }
-
-        objectInHand = null;
     }
 }
---
+++ Horror-VR/Assets/Scripts/Keypress.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace control
+{
+
+    public class Keypress : MonoBehaviour
+    {
+        //Fetch the Animator
+        Animator m_Animator;
+        // Use this for deciding if the GameObject can jump or not
+        public static bool keypress;
+        
+
+        void Start()
+        {
+            //This gets the Animator, which should be attached to the GameObject you are intending to animate.
+            m_Animator = gameObject.GetComponent<Animator>();
+            // The GameObject cannot jump
+            keypress = false;
+        }
+
+        void Update()
+        {
+
+            //If the GameObject is not jumping, send that the Boolean “Jump” is false to the Animator. The jump animation does not play.
+            if (keypress == false)
+            {
+                m_Animator.SetBool("keypress", false);
+            }
+
+
+            //The GameObject is jumping, so send the Boolean as enabled to the Animator. The jump animation plays.
+            if (keypress == true)
+            {
+                m_Animator.SetBool("keypress", true);
+                Debug.Log(keypress);
+            }
+        }
+    }
+}
