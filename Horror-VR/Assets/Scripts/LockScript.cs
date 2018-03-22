0b7c4ac51119737f96f1a3d5a4c7daa60db727ed
David Fall <David.fall@students.plymouth.ac.uk>
2018-03-22 05:04:45 +0000

    PuzzleNo.2
    added sounds
    puzzle 2
    creepy girl
    post processing

---
+++ Horror-VR/Assets/Scripts/LockScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockScript : MonoBehaviour {
+
+    private bool Locked;
+    public AudioSource Lock;
+    public AudioClip Unlocked;
+    public GameObject _Lock;
+    public Animation open;
+
+    void Start()
+    {
+        Locked = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "AcHands")
+        {
+            if (Locked == true )
+            {
+                Lock.Play();
+            }
+        }
+
+        if (other.tag == "Key")
+        {
+            Locked = false;
+            Lock.clip = Unlocked;
+            Lock.Play();
+            _Lock.SetActive(true);
+            Destroy(this, 5);
+            open.Play();
+
+        }
+    }
+
+
+}
