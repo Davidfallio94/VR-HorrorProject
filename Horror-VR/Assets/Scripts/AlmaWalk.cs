4a28d334ae850fc15aab4ca79e45a77e67a34ed6
David Fall <David.fall@students.plymouth.ac.uk>
2018-04-23 17:02:41 +0100

    AI script and alma attack added

---
+++ Horror-VR/Assets/Scripts/AlmaWalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AlmaWalk : MonoBehaviour
+{
+
+    public NavMeshAgent nav;
+    private Transform player;
+
+
+
+    void Start()
+    {
+       player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        nav.SetDestination(player.position);
+        NavMeshPath path = new NavMeshPath();
+        nav.CalculatePath(player.position, path);
+        nav.SetPath(path);
+    }
+}
+   
---
+++ Horror-VR/Assets/WalkingSoundTransitions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingSoundTransitions : StateMachineBehaviour {
+
+    public AudioSource almasound;
+    public AudioClip Walk;
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        almasound.clip = Walk;
+        almasound.Play();
+        almasound.loop = true;
+	}
+
+}
